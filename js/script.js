c61a9d36c2213342546347fbd07b5b1417d5961b
J Kacprzak <jan.kowalski@example.com>
2020-10-04 15:26:53 +0200

    Add titleClickHandler structure

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-'use strict'; //show errors
+'use strict'; // show errors
 
-function buttonClicked(buttonName){
-    console.log(buttonName + ' został kliknięty');
-  }
+//function titleClickHandler(){
+  //const links = document.querySelectorAll('.titles a');
+  //console.log(links);
+  //console.log(buttonTest.nodeName + ' został kliknięty');
+//}
+
+//const buttonTest = document.getElementById('button-test');
+
+//buttonTest.addEventListener('click', titleClickHandler);
+
+
+
+function titleClickHandler(){
+  console.log('Link was clicked');
   
-  function titleClickHandler(){
-    const links = document.querySelectorAll('.titles a');
-    console.groupCollapsed(links);
-  }
-  
-  const buttonTest = document.getElementById('button-test');
-  
-  buttonTest.addEventListener('click', titleClickHandler);
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+
+
+const links = document.querySelectorAll('.titles a');
+console.log(links);
+for (let link of links){
+  link.addEventListener('click', titleClickHandler);
+  console.log(link)
+  console.log()
